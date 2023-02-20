d650bf43d19a104c5ac7d1a72dd32aa1f3916cff
Michael Gilliland <mjg.py3@gmail.com>
2023-02-20 14:28:16 -0500

    Basic rendering

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, text, textarea)
+import Dict exposing (Dict)
+import Maybe exposing (Maybe)
+import String exposing (String)
+import Html exposing (Html, button, div, h1, text, textarea, input)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 import Json.Decode as Decode exposing (Decoder, field, int, map, map4, string)
 
 
@@ -13,12 +16,25 @@ main =
 
 type DefinitionValidity
     = ValidDefinition TrackerTopLevelSchema
-    | InvalidDefinition (Maybe Decode.Error)
+    | InvalidDefinition Decode.Error
+    | StartingOut
+
+
+type Value
+    = WholeNumber Int
+
+valueToString : Value -> String
+valueToString v =
+    case v of
+      WholeNumber n -> String.fromInt n
+
+type alias TrackingState =
+    Dict String Value
 
 
 type Stage
     = DefinitionStage String DefinitionValidity
-    | TrackerStage
+    | TrackerStage TrackerTopLevelSchema TrackingState
 
 
 type alias Model =
@@ -27,11 +43,15 @@ type alias Model =
 
 init : Model
 init =
-    DefinitionStage "" <| InvalidDefinition Nothing
+    DefinitionStage "" StartingOut
 
+type TrackMsg
+    = ApplyEffects (List Effect)
 
 type Msg
     = UpdateDefinition String
+    | CreateTracker TrackerTopLevelSchema
+    | TrackerMsg TrackerTopLevelSchema TrackingState TrackMsg
 
 
 update : Msg -> Model -> Model
@@ -39,34 +59,64 @@ update msg model =
     case msg of
         UpdateDefinition def ->
             case Decode.decodeString trackerTopLevelSchemaDecoder def of
-              Err e -> DefinitionStage def <| InvalidDefinition (Just e)
-              Ok d -> DefinitionStage def <| ValidDefinition d
+                Err e ->
+                    DefinitionStage def <| InvalidDefinition e
+
+                Ok d ->
+                    DefinitionStage def <| ValidDefinition d
+
+        CreateTracker def ->
+            TrackerStage def Dict.empty
+
+        TrackerMsg schema state tmsg ->
+            model
 
 
 view : Model -> Html Msg
 view model =
     case model of
         DefinitionStage def valid ->
+            viewEditTracker def valid
+
+        TrackerStage def state ->
+          viewTracker def state
+
+viewEditTracker : String -> DefinitionValidity -> Html Msg
+viewEditTracker def valid =
+    div []
+        [ div [] [ h1 [] [ text "New Tracker" ] ]
+        , div [] [ button [onClick (UpdateDefinition exampleDef) ] [text "(fill test)"] ]
+        , div [] [ textarea [ cols 40, rows 10, placeholder "...", onInput UpdateDefinition ] [] ]
+        , case valid of
+            ValidDefinition d ->
+                div []
+                    [ div [] [ text <| "Definition good: " ++ d.name ]
+                    , button [ onClick (CreateTracker d) ] [ text "Create!" ]
+                    ]
+
+            InvalidDefinition err ->
+                text ("Definition invalid: " ++ Decode.errorToString err)
+
+            StartingOut ->
+                text ""
+        ]
+
+
+viewTrackerComponent : TrackerSchema -> TrackingState -> Html TrackMsg
+viewTrackerComponent tracker state =
+    case tracker of
+      WholeNumberSchema s ->
+        div [] [text s.text, input [type_ "number", value (valueToString <| Maybe.withDefault s.default (Dict.get s.id state) )] []]
+      Group s ->
+        div [] (List.map (\i -> viewTrackerComponent i state) s.items)
+      Action s ->
+        button [onClick (ApplyEffects s.effects) ] [text s.text]
+
+viewTracker : TrackerTopLevelSchema -> TrackingState -> Html Msg
+viewTracker schema state =
             div []
-                [ div [] [ h1 [] [ text "New Tracker" ] ]
-                , div [] [ textarea [ cols 40, rows 10, placeholder "...", onInput UpdateDefinition ] [] ]
-                , case ( def, valid ) of
-                    ( "", _ ) ->
-                        text ""
-
-                    ( _, ValidDefinition d) ->
-                        text <| "Definition good: " ++ d.name
-
-                    ( _, InvalidDefinition (Just err)) ->
-                        text ("Definition invalid: " ++ Decode.errorToString err)
-                    ( _, InvalidDefinition Nothing) ->
-                        text ("Definition invalid")
-                ]
-
-        TrackerStage ->
-            div []
-                [ text "tracking" ]
-
+                [ h1 [] [ text schema.name ]
+                , Html.map (TrackerMsg schema state) <| viewTrackerComponent schema.tracker state]
 
 
 -- Tracker Schema
@@ -89,40 +139,96 @@ effectDecoder =
 type Effect
     = RestoreDefaults { items : List EffectTarget }
 
+
 groupDecoder : Decoder TrackerSchema
 groupDecoder =
-    Decode.map (\items -> Group {items = items}) <| field "items" (Decode.list trackerSchemaDecoder)
+    Decode.map (\items -> Group { items = items }) <| field "items" (Decode.list trackerSchemaDecoder)
 
-resetActionDecoder : Decoder TrackerSchema
-resetActionDecoder =
+
+actionDecoder : Decoder TrackerSchema
+actionDecoder =
     Decode.map2
-        (\text effects -> ResetAction {text=text, effects=effects})
+        (\text effects -> Action { text = text, effects = effects })
         (field "text" string)
         (field "effects" (Decode.list effectDecoder))
+
 
 wholeNumberDecoder : Decoder TrackerSchema
 wholeNumberDecoder =
     Decode.map3
-        (\text default id -> WholeNumber {text=text, default=default, id=id})
+        (\text default id -> WholeNumberSchema { text = text, default = default, id = id })
         (field "text" string)
-        (field "default" Decode.int)
-        (field "id" (Decode.maybe string))
+        (field "default" (Decode.map WholeNumber Decode.int))
+        (field "id" string)
+
 
 trackerSchemaDecoder : Decoder TrackerSchema
 trackerSchemaDecoder =
-    Decode.lazy <| \_ -> Decode.oneOf [groupDecoder, resetActionDecoder, wholeNumberDecoder]
+    Decode.lazy <| \_ -> Decode.oneOf [ groupDecoder, actionDecoder, wholeNumberDecoder ]
+
 
 type TrackerSchema
     = Group { items : List TrackerSchema }
-    | ResetAction { text : String, effects : List Effect }
-    | WholeNumber { text : String, default : Int, id : Maybe String }
+    | Action { text : String, effects : List Effect }
+    | WholeNumberSchema { text : String, default : Value, id : String }
 
 
 trackerTopLevelSchemaDecoder : Decoder TrackerTopLevelSchema
 trackerTopLevelSchemaDecoder =
     Decode.map2 TrackerTopLevelSchema (field "name" string) (field "tracker" trackerSchemaDecoder)
 
+
 type alias TrackerTopLevelSchema =
     { name : String
     , tracker : TrackerSchema
     }
+
+exampleDef : String
+exampleDef = """
+{
+  "name": "Dominion Turn Tracker",
+  "tracker": {
+    "type": "group",
+    "items": [
+      {
+        "type": "action",
+        "text": "Reset",
+        "effects": [
+          {
+            "type": "restore-defaults",
+            "items": [
+              {
+                "id": "a"
+              },
+              {
+                "id": "b"
+              },
+              {
+                "id": "em"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "type": "number",
+        "text": "Actions",
+        "default": 1,
+        "id": "a"
+      },
+      {
+        "type": "number",
+        "text": "Buys",
+        "default": 1,
+        "id": "b"
+      },
+      {
+        "type": "number",
+        "text": "Extra Money",
+        "default": 0,
+        "id": "em"
+      }
+    ]
+  }
+}
+              """
